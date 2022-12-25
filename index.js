ae1b2dfe92af462f446b369aaa1466f395d33dca
fasihkhan754 <115379095+fasihkhan754@users.noreply.github.com>
2022-12-25 13:22:08 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,62 @@
+#! /usr/bin/node env
+import inquirer from 'inquirer';
+const answer = await inquirer.prompt([
+    {
+        type: "input",
+        name: "username",
+        message: "Enter Your UserID : "
+    },
+    {
+        type: "number",
+        name: "pincode",
+        message: "Enter Your Pin : "
+    },
+    {
+        type: "list",
+        name: "AccountType",
+        choices: ["Current Account", "Saving Account"],
+        message: "Choose Account Type : "
+    },
+    {
+        type: "list",
+        name: "Transaction",
+        choices: ["Withdraw", "FAstCash"],
+        message: "Select Transaction : ",
+        when(answer) {
+            return answer.AccountType;
+        }
+    },
+    {
+        type: "list",
+        name: "Ammount",
+        choices: [1000, 2000, 3000, 4000, 5000],
+        message: "Select Transaction : ",
+        when(answer) {
+            return answer.AccountType = "FAstCash";
+        }
+    },
+    {
+        type: "list",
+        name: "Ammount",
+        choices: [500, 1000, 1500, 2000, 2500, 3000],
+        message: "Select Transaction : ",
+        when(answer) {
+            return answer.AccountType = "Withdraw";
+        }
+    }
+]);
+if (answer.username && answer.pincode) {
+    var remaining;
+    const Balance = 1000000;
+    console.log("TOTAL BALANCE REMAINING :" + Balance);
+    const entrd = answer.Ammount;
+    console.log(entrd);
+    if (Balance > entrd) {
+        remaining = Balance - entrd;
+        console.log("Remaining Balance : " + remaining);
+    }
+    else {
+        console.log("Insufficient Balance");
+    }
+}
+//console.log(answer.username);
